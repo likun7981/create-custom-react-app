71b5254c6bdf125de8355c3db97f2b12738ea43c
Lee <likun7981@gmail.com>
2017-09-13 18:30:53 +0800

    support remote,global package,local path

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # create-custom-react-app
-create a custom template react app, mainly a create-react-app wrapper.
+> create a custom template react app
+
+This is a `create-react-app` wrapper , so you can use anything that you can use in `create-react-app`, more detail use , you can see [create-react-app-user-guide](https://github.com/facebookincubator/create-react-app#user-guide)
+
+## Install
+```bash
+# You must install create-react-app
+$ npm install -g create-react-app
+
+$ npm install -g create-custom-react-app
+```
+
+# Usage
+```bash
+$ create-custom-react-app --help
+
+  Usage: create-custom-react-app <project-directory> [options]
+
+
+  Options:
+
+    -t, --template [template-path-or-name]  create react app use custom template
+    -h, --help                              output usage information
+
+  Other options are the same as create-react-app
+```
+
+The `template` can use the following three types:
+
+* Remote git repo from `github`, 
+```bash
+$ create-custom-react-app -t <username>/<repo-name>  <project-name>
+```
+
+example:
+```bash
+$ create-custom-react-app -t likun7981/test-git test-remote-project
+```
+
+* Global npm package like [Yeoman](https://github.com/yeoman/yeoman), the prefix is `react-app-template-<customname>`, example:
+
+```bash
+# Install the template with global
+$ npm install -g react-app-template-simple
+
+$ create-custom-react-app -t simple test-global-npm-package-project
+```
+
+* Local path template,  example
+```bash
+$ create-custom-react-app -t relative-or-absolute-local-path test-local-path-project
+```
+
+## Custom config  
+You can use [react-app-rewired](https://github.com/timarney/react-app-rewired) to custom config,
+more config package you can see [react-app-rewire-contrib](https://github.com/osdevisnot/react-app-rewire-contrib)
+
+## Why
+Because we want to customize the template, but you don't want to change [create-react-app](https://github.com/facebookincubator/create-react-app) all the time.
+
+
+## License 
+
+MIT, Lee<<likun7981@gmail.com>>
---
+++ index.js
@@ -0,0 +1,119 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const spawn = require("cross-spawn");
+const chalk = require("chalk");
+const fs = require("fs");
+const os = require("os");
+const ora = require("ora");
+const downloadGithubRepo = require("download-github-repo");
+const path = require("path");
+
+const packageJson = require("./package.json");
+
+const templateNpmPackagePrefix = "react-app-template-";
+
+const program = new commander.Command(packageJson.name)
+  .usage(`${chalk.green("<project-directory>")} [options]`)
+  .option(
+    "-t, --template [template-path-or-name]",
+    "create react app use custom template"
+  )
+  .allowUnknownOption()
+  .on("--help", () => {
+    console.log();
+    console.log(
+      `  Other options are the same as ${chalk.green("create-react-app")}`
+    );
+    spawn.sync("create-react-app", ["--help"], { stdio: "inherit" });
+  })
+  .parse(process.argv);
+
+const args = program.args;
+
+const rawArgs = program.rawArgs.slice(2);
+
+let template;
+
+// Hidden option for create-react-app
+// You can see https://qurl.com/ffphl
+if (program.template) {
+  template = program.template;
+} else if (args.length === 2) {
+  template = args[0];
+  rawArgs.splice(rawArgs.indexOf(template), 1);
+}
+if (template) {
+  handleTemplate(template)
+    .then(finallTemplate => {
+      rawArgs.push(`--internal-testing-template=${finallTemplate}`);
+      createReactApp(rawArgs);
+    })
+    .catch(error => {
+      console.log(error);
+      process.exit(0);
+    });
+} else {
+  createReactApp(rawArgs);
+}
+
+function createReactApp(args) {
+  const result = spawn.sync("create-react-app", args, { stdio: "inherit" });
+  if (result.error) {
+    console.log(result.error.message);
+  }
+}
+
+function isLocalPath(template) {
+  return /^[./]|(^[a-zA-Z]:)/.test(template);
+}
+
+function globalNpmTemplatePackage(name) {
+  let fullPath;
+  if (name.indexOf(templateNpmPackagePrefix) === -1) {
+    name = templateNpmPackagePrefix + name;
+  }
+  fullPath = path.join(__dirname, "../", name);
+  return {
+    isExists: fs.existsSync(fullPath),
+    path: fullPath,
+    name: name
+  };
+}
+
+function handleTemplate(template) {
+  return new Promise((resolve, reject) => {
+    console.log();
+    if (isLocalPath(template)) {
+      console.log(`Local template ${chalk.green(template)}`);
+      return resolve(template);
+    }
+    if (template.indexOf("/") !== -1) {
+      console.log(`Start download template ${chalk.green(template)}`);
+      const spinner = ora(`Downloading template ${chalk.green(template)}...`);
+      console.log();
+      spinner.start();
+      const tmp = path.join(
+        os.tmpdir(),
+        "react-app-template-",
+        `${new Date().valueOf()}`
+      );
+      return downloadGithubRepo(template, tmp, error => {
+        spinner.stop();
+        if (error) {
+          return reject(`Download error: ${error}`);
+        }
+        console.log(`Download ${chalk.green(template)} success, start create`);
+        return resolve(tmp);
+      });
+    }
+    const result = globalNpmTemplatePackage(template);
+    if (result.isExists) {
+      console.log(
+        `Find global npm template package ${chalk.green(result.name)}`
+      );
+      return resolve(result.path);
+    }
+    return reject(`can't resolve template ${chalk.red(template)}`);
+  });
+}
